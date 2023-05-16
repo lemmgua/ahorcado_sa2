7b3d78fae43f91dd74c6d6e5aa9d358b42bbc77d
lemmgua <alejandrozubirifunes@gmail.com>
2023-05-16 14:46:50 +0200

    leer datos

--- src/database.py
+++ src/database.py
@@ -2,17 +2,27 @@ import firebase_admin
 from firebase_admin import firestore
 from firebase_admin import credentials
 
-cred = credentials.Certificate(r"C:\Users\aleja\OneDrive\Documentos\.Code\hanged-man-sa2-firebase-adminsdk-b0ie6-95cbc7f745.json")
-app = firebase_admin.initialize_app(cred)
-db = firestore.client()
+def InsertarPuntuacion(nombre: str, puntos: int, mayorRacha: int) -> any:
+    cred = credentials.Certificate(r"C:\Users\aleja\OneDrive\Documentos\.Code\hanged-man-sa2-firebase-adminsdk-b0ie6-95cbc7f745.json")
+    app = firebase_admin.initialize_app(cred)
+    db = firestore.client()
 
-def InsertarPuntuacion(nombre: str, puntos: int, mayorRacha: int) -> None:
     data = {
         u"mayor-racha": mayorRacha,
         u"nombre": nombre,
         u"score": puntos
     }
-    doc_ref = db.collection(u"registros-jugadores").add(data)
+
+    return db.collection(u"registros-jugadores").add(data)
+
+def LeerDatos(nombre: str) -> dict:
+    cred = credentials.Certificate(r"C:\Users\aleja\OneDrive\Documentos\.Code\hanged-man-sa2-firebase-adminsdk-b0ie6-95cbc7f745.json")
+    app = firebase_admin.initialize_app(cred)
+    db = firestore.client()
+
+    doc = db.collection(u"registros-jugadores").where(u"nombre", u"==", nombre)
+
+    return doc.get()[0].to_dict()
 
 if __name__ == "__main__":
-    InsertarPuntuacion("leon", 5, 5)
+    print(LeerDatos("leon"))
