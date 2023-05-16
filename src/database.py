b65482313da86fc31c280d81a3ef6f7d76eb6f76
lemmgua <alejandrozubirifunes@gmail.com>
2023-05-16 14:31:17 +0200

    se pueden insertar datos

--- src/database.py
+++ src/database.py
@@ -1,11 +1,18 @@
-import firebase_admin, os
+import firebase_admin
+from firebase_admin import firestore
 from firebase_admin import credentials
-from firebase_admin import db
 
-GOOGLE_APPLICATION_CREDENTIALS = os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
-
-cred = credentials.Certificate(GOOGLE_APPLICATION_CREDENTIALS)
+cred = credentials.Certificate(r"C:\Users\aleja\OneDrive\Documentos\.Code\hanged-man-sa2-firebase-adminsdk-b0ie6-95cbc7f745.json")
 app = firebase_admin.initialize_app(cred)
+db = firestore.client()
 
-ref = db.reference("/registros-jugadores")
-print(ref.get())
+def InsertarPuntuacion(nombre: str, puntos: int, mayorRacha: int) -> None:
+    data = {
+        u"mayor-racha": mayorRacha,
+        u"nombre": nombre,
+        u"score": puntos
+    }
+    doc_ref = db.collection(u"registros-jugadores").add(data)
+
+if __name__ == "__main__":
+    InsertarPuntuacion("leon", 5, 5)
